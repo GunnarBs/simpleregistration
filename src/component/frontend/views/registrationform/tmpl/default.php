8726dd1bab96157468ec5dbee67e4954bacfd229
zanardi <f.abeni@gibilogic.com>
2014-01-26 21:19:35 +0100

    Refs #6559 - add mootools validation

--- src/component/frontend/views/registrationform/tmpl/default.php
+++ src/component/frontend/views/registrationform/tmpl/default.php
@@ -1,32 +1,31 @@
-<?php defined('_JEXEC') or die('The way is shut!');
+<?php
 /**
-* @version		  $Id: views/registrationform/tmpl/default.php 2013-03-17 15:35:00Z zanardi $
+* @version		  frontend/views/registrationform/tmpl/default.php 2014-01-26 20:14:00 UTC zanardi
 * @package		  GiBi SimpleRegistration
-* @author       GiBiLogic
-* @authorEmail  info@gibilogic.com
+* @author       GiBiLogic <info@gibilogic.com>
 * @authorUrl    http://www.gibilogic.com
-* @copyright	  Copyright (C) 2011-2013 GiBiLogic snc. All rights reserved.
-* @license		  GNU/GPL v2 or later
+* @copyright	  Copyright (C) 2011-2014 GiBiLogic snc. All rights reserved.
+* @license		  GNU/GPL v3 or later
 */
+defined('_JEXEC') or die();
 ?>
-
+<div class="registration">
 <?php if ( $this->params->def( 'show_page_title', 1 ) ) : ?>
-	<h1 class="componentheading<?php echo $this->escape($this->params->get('pageclass_sfx','')); ?>">
+	<h1 class="title<?php echo $this->escape($this->params->get('pageclass_sfx','')); ?>">
 		<?php echo $this->escape($this->params->get('page_title')); ?>
 	</h1>
 <?php endif; ?>
-
-<div class="registration">
-	<form action="<?php echo JRoute::_($this->_url."&task=save") ?>" method="post" name="adminForm" id="adminForm" class="form-validate form-horizontal">
+	<p><?php echo JText::_('COM_SIMPLEREGISTRATION_NOTE'); ?></p>
+	<form action="<?php echo JRoute::_($this->_url.'&task=save') ?>" method="post" name="adminForm" id="adminForm" class="<?php if ($this->params->get('mootools', 1)) echo 'form-validate '; ?>form-horizontal">
 		<div class="control-group">
 			<div class="control-label">
 				<label class="label-left" for="email"><?php echo JText::_('JGLOBAL_EMAIL'); ?>: </label>
 			</div>
 			<div class="controls">
-				<input type="text" name="email" id="email" class="validate-email required">
+				<input type="text" name="email" id="email" class="<?php if ($this->params->get('mootools', 1)) echo 'validate-email '; ?>required">
 			</div>
 		</div>
-		<?php if( $this->params->get('requestpassword') ) : ?>
+		<?php if($this->params->get('requestpassword')) : ?>
 		<div class="control-group">
 			<div class="control-label">
 				<label class="label-left" for="password"><?php echo JText::_('JGLOBAL_PASSWORD'); ?>: </label>
@@ -41,6 +40,6 @@
 				<?php echo JText::_('JREGISTER') ?>
 			</button>
 		</div>
-		<?php echo JHTML::_( 'form.token' ); ?>
+		<?php echo JHTML::_('form.token'); ?>
 	</form>
 </div>
--- src/component/frontend/views/registrationform/view.html.php
+++ src/component/frontend/views/registrationform/view.html.php
@@ -1,57 +1,63 @@
-<?php defined('_JEXEC') or die( 'The way is shut' );
+<?php
 /**
-* @version		  $Id: views/registrationform/view.html.php 2013-03-17 14:31:00Z zanardi $
+* @version		  frontend/views/registrationform/view.html.php 2014-01-26 20:14:00 UTC zanardi
 * @package		  GiBi SimpleRegistration
-* @author       GiBiLogic
-* @authorEmail  info@gibilogic.com
+* @author       GiBiLogic <info@gibilogic.com>
 * @authorUrl    http://www.gibilogic.com
-* @copyright	  Copyright (C) 2011-2013 GiBiLogic snc. All rights reserved.
-* @license		  GNU/GPL v2 or later
+* @copyright	  Copyright (C) 2011-2014 GiBiLogic snc. All rights reserved.
+* @license		  GNU/GPL v3 or later
 */
-
-jimport( 'joomla.application.component.view');
-jimport( 'joomla.application.component.helper');
+defined('_JEXEC') or die();
+jimport('joomla.application.component.view');
+jimport('joomla.application.component.helper');
 
 class SimpleregistrationViewRegistrationform extends JViewLegacy
 {
-	protected $_url;
 
-	function __construct($config=array())
-	{
-		$this->_url = 'index.php?option=com_simpleregistration&view=registrationform';
-		parent::__construct($config);
-	}
+    protected $_url;
 
-	function display($tpl = null)
-	{
-		$app =& JFactory::getApplication();
-		$pathway	=& $app->getPathway();
-		$params 	=& $app->getParams();
-		$document	=& JFactory::getDocument();
-		$user		=& JFactory::getUser();
+    public $params;
 
-		if ( $user->guest ) {
-			$params->set('page_title', JText::_('REGISTRATION_FORM_TITLE') );
-		} else {
-			$params->set('page_title', JText::_('ALREADY_REGISTERED_FORM_TITLE') );
-		}
+    public function __construct($config = array())
+    {
+        parent::__construct($config);
+        $this->_url = 'index.php?option=com_simpleregistration&view=registrationform';
+    }
 
-		$menus	= &JSite::getMenu();
-		$menu	= $menus->getActive();
-		if( is_object( $menu ) ) {
-			$menu_params = new JRegistry( $menu->params );
-			if( $menu_params->get( 'page_title') ) {
-				$params->set('page_title', $menu_params->get( 'page_title') );
-			}
-		}
+    function display($tpl = null)
+    {
+        $app = JFactory::getApplication();
+        $pathway = $app->getPathway();
+        $params = $app->getParams();
+        $document = JFactory::getDocument();
+        $user = JFactory::getUser();
 
-		$document->setTitle( $params->get( 'page_title' ) );
-		// TODO add JavaScript validation from Bootstrap or JQuery
-		$pathway->addItem(JText::_('New'), '');
+        $params->set('page_title', JText::_('COM_SIMPLEREGISTRATION_TITLE'));
 
-		JHTML::_('behavior.formvalidation');
+        $jsite = new JSite();
+        $menus = $jsite->getMenu();
+        $menu = $menus->getActive();
 
-		$this->assignRef('params', $params);
-		parent::display($tpl);
-	}
+        if (is_object($menu))
+        {
+            $menu_params = new JRegistry($menu->params);
+            if ($menu_params->get('page_title'))
+            {
+                $params->set('page_title', $menu_params->get('page_title'));
+            }
+        }
+
+        $document->setTitle($params->get('page_title'));
+        $pathway->addItem(JText::_('New'), '');
+
+        $comparams = JComponentHelper::getParams('com_simpleregistration');
+        if ($comparams->get('mootools'))
+        {
+            JHTML::_('behavior.formvalidation');
+        }
+
+        $this->assignRef('params', $params);
+        parent::display($tpl);
+    }
+
 }
